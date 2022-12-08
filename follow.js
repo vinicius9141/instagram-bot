c37178055205c2680630579d2fcd002d6ff45430
Vinicius Oliveira <50337315+vinicius9141@users.noreply.github.com>
2022-12-08 13:00:27 -0300

    initial commit

---
+++ follow.js
@@ -0,0 +1,13 @@
+let contador = 0;
+
+document.querySelectorAll('.sqdOP.L3NKy.y3zKF').forEach((item, index) => {
+  setTimeout(() => {
+		if(!item.classList.contains('._8A5w5')){
+        	item.click();
+        	contador++;
+			console.log(`O BOT já seguiu ${contador} pessoa(s)!`);
+		} else { 
+			console.log('Você já segue essa pessoa.');
+    }
+  }, index * 20000);
+});
